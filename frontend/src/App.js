c40c05893aadc3d4bbe514c12cd237bc70ad8917
Amha Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2025-08-29 00:47:40 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import LoginPage from './Pages/Auth/loginPage';
 import LogoutPage from './Pages/Auth/logoutPage';
 import TaskPage from './Pages/Task/createTaskPage';
 import HomePage from './Pages/homePage';
-//import AdminRolesPage from './Pages/User/adminRolesPage';
+// import AdminRolesPage from './Pages/User/adminRolesPage';
 import ForgotPasswordPage from './Pages/Auth/forgotPasswordPage';
 import ResetPasswordPage from './Pages/Auth/resetPasswordPage';
 import ConfirmEmail from './Components/authComponents/confirmEmail';
@@ -33,7 +33,7 @@ import SubscriptionPage from './Pages/Subscription/subscriptionPage';
 import PurchasePage from './Pages/Subscription/purchasePage';
 import DashboardPage from './Pages/Subscription/dashboardPage';
 import RegistrationFlow from './Pages/Subscription/registrationFlow';
-import AuthHandoff from './Components/authComponents/authHandoff';
+// import AuthHandoff from './Components/authComponents/authHandoff';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { connectSocket } from "./socket";
@@ -78,7 +78,7 @@ const App = () => {
       <ConditionalNavBar />
       <div className="pt-16"> {/* Pushes content below navbar */}
         <Routes>
-          <Route path="/" element={isLoggedIn ? <Navigate to="/landing" /> : <Navigate to="/login" />} />
+          <Route path="/" element={isLoggedIn ? <Navigate to="/home" /> : <Navigate to="/landing" /> } />
           <Route path="/landing" element={<LandingPage />} />
           <Route path="/register-sub" element={<RegisterSubPage/>}/>
           <Route path="/subscription" element={< SubscriptionPage/>}/>
@@ -92,7 +92,7 @@ const App = () => {
           <Route path="/logout" element={<LogoutPage />} />
           <Route path="/forgot-password" element={<ForgotPasswordPage />} />
           <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
-          <Route path="/confirm-email/:confirmationCode" element={<ConfirmEmail />} />
+          <Route path="/confirm-email/:tenantId/:confirmationCode" element={<ConfirmEmail />} />
           <Route path="/unauthorized" element={<UnauthorizedPage />} />
           <Route path="/import-calendar" element={<MainLayout><CalendarImport /></MainLayout>} />
           {/* <Route 
